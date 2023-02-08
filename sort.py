8ca5debd3a172b3d3043509326ced659d19694fc
tylersb <tylersb@gmail.com>
2023-02-07 22:10:11 -0800

    finishes bonus algorithm

--- sort.py
+++ sort.py
@@ -6,8 +6,11 @@ def is_sorted(ls):
   return True
 
 def selection_sort(li):
-    # implement your algorithm here
-     pass
+  for i in range(len(li) - 1):
+    for j in range(i, len(li) - 1):
+      if li[j] < li[i]:
+        li[i], li[j] = li[j], li[i]
+    
 
 # for testing
 li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
